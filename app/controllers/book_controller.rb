ad9f39d09372430a7c2d3281ab7d7d094ff7ffe3
Kitton <alex@digitalorigin.com>
2017-09-25 20:29:53 +0200

    upload and save book

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,8 @@
+class BookController < ApplicationController
+  def create
+    uploaded_io = params[:book]
+    File.open(Rails.root.join('public', 'upload_books', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
+end
