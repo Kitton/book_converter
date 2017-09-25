c6f5add43a174be9542403ed9f54d1cc70dc4edf
Kitton <alex@digitalorigin.com>
2017-09-25 21:20:20 +0200

    make book accessible

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @book_to_show = flash[:book_to_show]
+    if flash[:book_to_show].present?
+      @book_to_show = flash[:book_to_show]
+      @book_path = "upload_books/#{@book_to_show}"
+    end
   end
 end
